1e3229e2bbb5f6b798728a3ed6301f62675a8cd5
matt <matthew.dobrowolski5@gmail.com>
2018-08-28 19:43:06 -0600

    there

--- src/store.js
+++ src/store.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
+let cardApi = Axios.create({
+  baseURL:'https://inspire-server.herokuapp.com/cards',
+  timout: 3000
+})
+
+
+
 export default new Vuex.Store({
   state: {
+    game:[]
 
   },
   mutations: {
