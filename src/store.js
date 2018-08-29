e25d8168fdf016fbe435c46604195fc54873e0d3
mattdobs3125 <matthew.dobrowolski5@gmail.com>
2018-08-28 23:49:38 -0600

    got that object!!!!!

--- src/store.js
+++ src/store.js
@@ -13,13 +13,34 @@ let cardApi = Axios.create({
 
 export default new Vuex.Store({
   state: {
-    game:[]
+    game:{}
 
   },
   mutations: {
-
+  setG (state, data){
+    state.game = data
+    console.log(data)
+  }
   },
   actions: {
+    deckSelection({dispatch,commit},choice){
+      let deckChoice 
+      switch(choice){
+        case 1:
+        deckChoice = {"playerName":"robotDeck","opponents": 1 ,"set":1}
+        break
+        case 2:
+        deckChoice = {"playerName":"catDeck","opponents": 1 ,"set":4}
+        break
+        
+              }
+              cardApi.post('/',{"ganeConfig" : deckChoice})
+              .then(res => commit('setG' , res.data))
+
+
 
   }
+
+
+}
 })
---
+++ src/views/cards.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="game">
+    <div class="selection">
+      <h1>Choose Your Deck</h1>
+      <button @click='deckSelection(1)'></button>
+      <img src="https://robohash.org/YOUR-TEXT.png?set=set1">
+
+    </div>
+
+
+
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'cards',
+  components: {},
+  methods:{
+deckSelection(deckChoice){
+  console.log(deckChoice)
+  this.$store.dispatch("deckSelection",deckChoice)
+}
+  }
+}
+</script>
